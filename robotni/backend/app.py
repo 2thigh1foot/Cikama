1d30fec1ace4e1e83c896414825378b9bf069c7d
kateemery <kemery5437@scrippscollege.edu>
2019-10-15 13:01:15 -0700

    added Flask backend for handling user logins

---
+++ robotni/backend/app.py
@@ -0,0 +1,48 @@
+from flask import Flask, request
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+app.config["MONGO_URI"] = "mongodb+srv://kemery:sunnypool@robotni-free-jvqvi.mongodb.net/test?retryWrites=true&w=majority"
+mongo = PyMongo(app)
+
+# help(mongo.db.create_collection)
+mongo.db.create_collection('Users')
+for a in dir(mongo.db):
+    print (a)
+
+@app.route("/")
+def home_page():
+    print (dir(mongo.db))
+
+# @app.route("/user/<username>")
+# def user_profile(username):
+#     user = mongo.db.users.find_one_or_404({"_id": username})
+#     return str(user)
+
+# @app.route('/hello')
+# def hello():
+#   return 'Hello, greetings from different endpoint'
+
+# #adding variables
+# @app.route('/user/<username>')
+# def show_user(username):
+#   #returns the username
+#   return 'Username: %s' % username
+
+# # non integer post_id throws 404
+# @app.route('/post/<int:post_id>')
+# def show_post(post_id):
+#   #returns the post, the post_id should be an int
+#   return str(post_id)
+
+
+# @app.route('/login', methods=['GET','POST'])
+# def login():
+#   if request.method == 'POST':
+#     data = request.form
+#     return 'POST'
+#   elif request.method == 'GET':
+#     #serve login page
+#     return "GET"
+#     # serve_login_page()
---
+++ robotni/backend/models.py
@@ -0,0 +1,17 @@
+from flask_login import UserMixin
+import app
+
+db = app.app.config.get('MONGODB_SETTINGS')['db']
+
+class User(UserMixin, db.Document):
+    meta = {'collection': '<---YOUR_COLLECTION_NAME--->'}
+    email = db.StringField(max_length=30)
+    password = db.StringField()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.objects(pk=user_id).first()
+
+class RegForm(FlaskForm):
+    email = StringField('email',  validators=[InputRequired(), Email(message='Invalid email'), Length(max=30)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=20)])
