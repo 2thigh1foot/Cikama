2055261d5f2ec4079e7b1f8424bf33d906965712
Max Kunz <maximilliankunz@gmail.com>
2019-11-02 11:05:47 -0700

    Removed a lot of unnecessary files! Woo!

--- robotni/react/old_frontend/react-client/src/index.jsx
+++
@@ -1,5 +0,0 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import App from './Components/App';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
-ReactDOM.render(<App />, document.getElementById('app'));
