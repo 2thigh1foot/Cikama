bc7616e14319faf89083cc916cad45ea2101ece4
kateemery <kemery5437@scrippscollege.edu>
2019-11-13 13:50:00 -0800

    implemented user signup with post functionality to mongo backend

--- robotni/react/backend/routes/users.js
+++ robotni/react/backend/routes/users.js
@@ -10,16 +10,31 @@ router.route('/').get((req, res) => {
 
 // at localhost:5000/routes/add we will try to create a new user
 router.route('/add').post((req, res) => {
+    
+    console.log(req.body);
+
     const username = req.body.username;
     const password = req.body.password;
     const location = Number(req.body.location);
-    const plants = Array(req.body.plants);
+    const plants = Array(['hi'])
+    // req.body.plants;
+    
+
+    // Server side logging
+    console.log('USERNAME: ' + username);
+    console.log('PASSWORD: ' + password);
+    console.log('LOCATION: ' + location);
+    console.log('PLANTS ARRAY: ' + plants);
+
 
     const newUser = new User({username, password, location, plants});
 
     newUser.save()
         .then(() => res.json('User added!'))
-        .catch(err => res.status(400).json('Error: ' + err));
+        .catch( (err) => { 
+            console.log(err);
+            res.status(400).json('Error: ' + err);
+        });
 });
 
 module.exports = router;
