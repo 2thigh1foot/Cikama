43cede2aa194c19988f0f24ec216a174b5ef688c
kateemery <kemery5437@scrippscollege.edu>
2019-12-10 14:02:48 -0800

    removed robotni

--- robotni/react/src/Routes.js
+++
@@ -1,6 +0,0 @@
-import Login from './Login.js'
-import App from './App.js'
-import Signup from "./containers/Signup";
-import Zipcode from "./containers/Zipcode.js";
-
-<AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
